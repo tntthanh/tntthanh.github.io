046cab45945d7f2c0bfaa3aaffaa66fceaa83e21
tntthanh <63393350+tntthanh@users.noreply.github.com>
2020-08-02 10:27:40 +0700

    Initial commit

---
+++ js/myprojects.js
@@ -0,0 +1,17 @@
+var localHost;
+localHost=prompt("Enter localhost:",8888);
+console.log(localHost);
+localHost.toString();
+console.log(localHost);
+var a="/notebooks/1_Live%20Sketch%20Using%20Webcam.ipynb";
+var b="/notebooks/2_Finding%20Waldo.ipynb";
+var c="/notebooks/3_Pedestrian%20Detection.ipynb";
+var d="/notebooks/4_Car%20Detection.py.ipynb";
+var e="/notebooks/5_%20Shrimp%20Detection.ipynb";
+
+
+document.getElementsByClassName("link")[0].href="http://localhost:"+localHost+a;
+document.getElementsByClassName("link")[1].href="http://localhost:"+localHost+b;
+document.getElementsByClassName("link")[2].href="http://localhost:"+localHost+c;
+document.getElementsByClassName("link")[3].href="http://localhost:"+localHost+d;
+document.getElementsByClassName("link")[4].href="http://localhost:"+localHost+e;
